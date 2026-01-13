17db0ee03fcfe63a8627fce6a9f25d919714ea59
siva26272829 <109459225+siva26272829@users.noreply.github.com>
2026-01-13 20:09:21 +0530

    Add Supabase client initialization script

---
+++ supabase.js
@@ -0,0 +1,11 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+
+<script>
+  const SUPABASE_URL = "https://giekbwvblhzzllcrmyud.supabase.co";
+  const SUPABASE_ANON_KEY = "sb_publishable_7HV8Fi3n0_kbVWXX4ZTDxQ_OXCrAfzB";
+
+  window.supabase = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+  );
+</script>
